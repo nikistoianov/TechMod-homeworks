20daa457da663a8b83b9a62fd31951193d8e1611
Niki-home <nikmastera@gmail.com>
2017-10-16 01:10:48 +0300

    Lists Update

---
+++ Lists/ArrayManipulatorr/05.StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulatorr
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "print")
+            {
+                string[] arr = commandLine.Split();
+                switch (arr[0])
+                {
+                    case "add":
+                        nums.Insert(int.Parse(arr[1]), int.Parse(arr[2]));
+                        break;
+                    case "addMany":
+                        //List<int> elements = new List<int>(arr);
+                        int[] elements = new int[arr.Length - 2];
+                        for (int i = 2; i < arr.Length; i++)
+                        {
+                            elements[i - 2] = int.Parse((arr[i]));
+                        }
+                        nums.InsertRange(int.Parse(arr[1]), elements);
+                        break;
+                    case "contains":
+                        int id = nums.IndexOf(int.Parse(arr[1]));
+                        Console.WriteLine(id);
+                        break;
+                    case "remove":
+                        nums.RemoveAt(int.Parse(arr[1]));
+                        break;
+                    case "shift":
+                        break;
+                    case "sumPairs":
+                        break;
+
+                }
+                commandLine = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
--- Lists/LongestIncreasingSubsequence/04.StartUp.cs
+++ Lists/LongestIncreasingSubsequence/04.StartUp.cs
@@ -10,6 +10,46 @@ namespace LongestIncreasingSubsequence
     {
         static void Main()
         {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> result = GetLIS(arr);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static List<int> GetLIS(int[] arr)
+        {
+            List<List<int>> nums = new List<List<int>>();
+
+            int maxLenght = 1;
+
+            foreach (int item in arr)
+            {
+                int loopEnd = nums.Count;
+                bool itemAdded = false;
+                for (int i = 0; i < loopEnd; i++)
+                {
+                    int lastNum = nums[i].Count - 1;
+                    if (nums[i][lastNum++] < item)
+                    {
+                        nums.Add(new List<int>(nums[i].ToArray()));
+                        nums[nums.Count - 1].Add(item);
+                        itemAdded = true;
+                        maxLenght = Math.Max(maxLenght, lastNum + 1);
+                    }
+                }
+                if (!itemAdded)
+                {
+                    nums.Add(new List<int> { item });
+                }
+            }
+            
+            foreach (List<int> singleNums in nums)
+            {
+                if (singleNums.Count == maxLenght)
+                {
+                    return singleNums;
+                }
+            }
+            return new List<int>();
         }
     }
 }

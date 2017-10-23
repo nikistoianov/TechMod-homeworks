a3947932d47d97c14d60ca49af9ae86ee4d1140c
Nikolay <nikmastera@gmail.com>
2017-10-23 17:49:34 +0300

    ObjectsAndClasses Update

--- ObjectsAndClasses/BookLibraryModification/06.StartUp.cs
+++ ObjectsAndClasses/BookLibraryModification/06.StartUp.cs
@@ -5,30 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BookLibrary
+namespace BookLibraryModification
 {
     class Program
     {
         static void Main()
         {
-            Library myLibrary = new Library(){Name = "My books", Books = new List<Book>()};
-            
+            Library myLibrary = new Library() { Name = "My books", Books = new List<Book>() };
+
             int nums = int.Parse(Console.ReadLine());
             for (int i = 0; i < nums; i++)
             {
                 myLibrary.Books.Add(ConvertInputToBook(Console.ReadLine()));
             }
+            DateTime compareDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
             
-            var result = new SortedDictionary<string, double>();
-            foreach (string autor in myLibrary.Books.Select(x => x.Autor).Distinct())
+            foreach (var item in myLibrary.Books.Where(x => x.ReleaseDate > compareDate).OrderBy(x => x.ReleaseDate).ThenBy(x => x.Title))
             {
-                double sum = myLibrary.Books.Where(x => x.Autor == autor).Sum(x => x.Price);
-                result.Add(autor, sum);
-            }
-
-            foreach (var item in result.OrderByDescending(x => x.Value))
-            {
-                Console.WriteLine($"{item.Key} -> {item.Value:0.00}");
+                Console.WriteLine($"{item.Title} -> {item.ReleaseDate:dd.MM.yyyy}");
             }
         }
 
@@ -37,8 +31,12 @@ namespace BookLibrary
             string[] arr = inputString.Split();
             Book book = new Book()
             {
-                Title = arr[0], Autor = arr[1], Publisher = arr[2], ReleaseDate = DateTime.ParseExact(arr[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
-                ISBN = int.Parse(arr[4]), Price = double.Parse(arr[5])
+                Title = arr[0],
+                Autor = arr[1],
+                Publisher = arr[2],
+                ReleaseDate = DateTime.ParseExact(arr[3], "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                ISBN = int.Parse(arr[4]),
+                Price = double.Parse(arr[5])
             };
             return book;
         }

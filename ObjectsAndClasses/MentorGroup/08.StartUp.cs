21718e1245ac00ebd90ddff6d875219e901630b9
Nikolay <nikmastera@gmail.com>
2017-10-24 17:54:17 +0300

    ObjectsAndClasses Update

---
+++ ObjectsAndClasses/MentorGroup/08.StartUp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorGroup
+{
+    class Program
+    {
+        static void Main()
+        {
+            var students = new SortedDictionary<string, Student>();
+            string input = Console.ReadLine();
+            while (input != "end of dates")
+            {
+                InputStudentDates(students, input);
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "end of comments")
+            {
+                InputStudentComments(students, input);
+                input = Console.ReadLine();
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Key);
+                Console.WriteLine("Comments:");
+                foreach (var comment in student.Value.comments)
+                {
+                    Console.WriteLine($"- {comment}");
+                }
+                Console.WriteLine("Dates attended:");
+                foreach (var date in student.Value.dates.OrderBy(x => x))
+                {                    
+                    //string dateString = string.Format(date, "dd/MM/yyyy");
+                    Console.WriteLine($"-- {date.ToString("dd/MM/yyyy")}");
+                }
+            }
+        }
+
+        private static void InputStudentComments(SortedDictionary<string, Student> students, string input)
+        {
+            string[] arr = input.Split('-');
+            string studentName = arr[0], comment = arr[1];
+            if (students.ContainsKey(studentName))
+            {
+                //if (students[studentName].comments == null)
+                //{
+                //    students[studentName].comments = new List<string>();
+                //}
+                students[studentName].comments.Add(comment);
+            }
+        }
+
+        private static void InputStudentDates(SortedDictionary<string, Student> students, string input)
+        {
+            string[] arr = input.Split();
+            string studentName = arr[0];
+            List<DateTime> dates = new List<DateTime>();
+            if (arr.Length > 1)
+            {
+                dates = arr[1].Split(',').Select(x => DateTime.ParseExact(x, "dd/MM/yyyy", CultureInfo.InvariantCulture)).ToList();
+            }
+            //string[] dates = arr.Length > 1 ? arr[1].Split(',') : null;
+
+            if (!students.ContainsKey(studentName))
+            {
+                Student student = new Student() { comments = new List<string>()};
+                student.dates = dates;
+                //student.dates = dates.Select(x => DateTime.ParseExact(x, "dd/MM/yyyy", CultureInfo.InvariantCulture)).ToList();
+                students[studentName] = student;
+            }
+            else
+            {
+                var newDates = students[studentName].dates.Concat(dates).ToList();
+                students[studentName].dates = newDates;
+            }
+        }
+    }
+
+    public class Student
+    {
+        public List<string> comments { get; set; }
+        public List<DateTime> dates { get; set; }
+    }
+}
---
+++ ObjectsAndClasses/TeamworkProjects/09.StartUp.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamworkProjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Team> teams = new List<Team>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                //string input = Console.ReadLine();                
+                Console.WriteLine(CreateTeam(teams, Console.ReadLine()));                
+            }
+
+            string input = Console.ReadLine();
+            while (input != "end of assignment")
+            {
+                string errMsg = "";
+                if (!AppendMemberToTeam(teams, input, ref errMsg))
+                {
+                    Console.WriteLine(errMsg);
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var team in teams.Where(x => x.Members.Count > 0).OrderByDescending(x => x.Members.Count).ThenBy(x => x.Name))
+            {
+                Console.WriteLine(team.Name);
+                Console.WriteLine($"- {team.teamCreator}");
+                foreach (var member in team.Members.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {member}");
+                }
+            }
+
+            Console.WriteLine("Teams to disband:");
+            foreach (var team in teams.Where(x => x.Members.Count == 0).OrderBy(x => x.Name))
+            {
+                Console.WriteLine(team.Name);
+            }
+
+        }
+
+        private static bool AppendMemberToTeam(List<Team> teams, string input, ref string errMsg)
+        {
+            string[] arr = input.Split(new char[] { '-', '>'}, StringSplitOptions.RemoveEmptyEntries);
+            string member = arr[0], teamName = arr[1];
+            
+            int index = teams.FindIndex(delegate (Team x) { return x.Name == teamName; });
+            if (index < 0)
+            {
+                errMsg = $"Team {teamName} does not exist!";
+                return false;
+            }
+           
+            if (
+                teams.Exists(delegate (Team x) { return x.Members.IndexOf(member) > -1; }) ||
+                teams.Exists(delegate (Team x) { return x.teamCreator == member; })
+                )
+            {               
+                errMsg = $"Member {member} cannot join team {teamName}!";
+                return false;
+            }
+
+            teams[index].Members.Add(member);
+            return true;
+        }
+
+        private static string CreateTeam(List<Team> teams, string input)
+        {
+            string[] arr = input.Split('-');
+            string creator = arr[0], teamName = arr[1];
+            if (teams.Exists(delegate (Team x) { return x.Name == teamName; }))
+            {
+                return $"Team {teamName} was already created!";                
+            }
+
+            if (teams.Exists(delegate (Team x) { return x.teamCreator == creator; }))
+            {
+                return $"{creator} cannot create another team!";
+            }
+
+            Team team = new Team() {Name = teamName, teamCreator = creator, Members = new List<string>()};
+            teams.Add(team);
+            return $"Team {teamName} has been created by {creator}!";
+        }
+    }
+
+    public class Team
+    {
+        public string Name { get; set; }
+        public List<string> Members { get; set; }
+        public string teamCreator { get; set; }
+    }
+}

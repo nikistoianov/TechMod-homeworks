7dfe50ef59f909b04feb19ebe24717abb695a4c9
Nikolay <nikmastera@gmail.com>
2017-10-27 18:14:58 +0300

    Strings-and-Text-Processing-Exercise Update

---
+++ StringsAndTextProcessing/LettersChangeNumbers/08.StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            //string input = "z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z z2147483647z";
+            double sum = 0;
+            string[] arr = input.Split(new char[] { ' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in arr)
+            {
+                double stringVal = CalcStringVal(item);
+                sum += stringVal;
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+
+        private static double CalcStringVal(string text)
+        {            
+            char firstChar = text[0], lastChar = text[text.Length - 1];            
+            int firstIndex, lastIndex;
+            string middle = text.Substring(1, text.Length - 2);
+            int number = int.Parse(middle);
+            double result = (double)number;
+
+            //divide if uppercase
+            if (firstChar > 64 && firstChar < 91)
+            {
+                firstIndex = firstChar - 64;
+                result /= firstIndex;
+            }
+            else if (firstChar > 96 && firstChar < 123)
+            {
+                firstIndex = firstChar - 96;
+                result *= firstIndex;
+            }
+            else
+            {
+                return 0;
+            }
+
+            //subtract if uppercase
+            if (lastChar > 64 && lastChar < 91)
+            {
+                lastIndex = lastChar - 64;
+                result -= lastIndex;
+            }
+            else if (lastChar > 96 && lastChar < 123)
+            {
+                lastIndex = lastChar - 96;
+                result += lastIndex;
+            }
+
+            return result;
+        }
+    }
+}
--- StringsAndTextProcessing/MultiplyBigNumber/07.StartUp.cs
+++ StringsAndTextProcessing/MultiplyBigNumber/07.StartUp.cs
@@ -11,10 +11,43 @@ namespace MultiplyBigNumber
     {
         static void Main()
         {
-            BigInteger firstNum = BigInteger.Parse(Console.ReadLine());
+            //Console.WriteLine(MultiplyBigNumbers("9999", 9));
+            string firstNum = Console.ReadLine();
             byte secondNum = byte.Parse(Console.ReadLine());
-            var result = firstNum * secondNum;
+            string result = MultiplyBigNumbers(firstNum, secondNum);
             Console.WriteLine(result);
+        }
+
+        private static string MultiplyBigNumbers(string firstNum, byte secondNum)
+        {
+            if (secondNum == 0)
+            {
+                return "0";
+            }
+            firstNum = ReverseString(firstNum);
+            string result = "";
+            int waitingDigit = 0;
+            for (int i = 0; i < firstNum.Length; i++)
+            {
+                int firstDigit = firstNum[i] - 48;
+                //int secondDigit = i < secondNum.Length ? (secondNum[i] - 48) : 0;
+                int multiply = waitingDigit + firstDigit * secondNum;
+                int addDigit = multiply % 10;
+                result = addDigit + result;
+                waitingDigit = multiply / 10;
+            }
+            if (waitingDigit > 0)
+            {
+                result = waitingDigit + result;
+            }
+            return result.TrimStart('0');
+        }
+
+        private static string ReverseString(string text)
+        {
+            char[] charArr = text.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
         }
     }
 }

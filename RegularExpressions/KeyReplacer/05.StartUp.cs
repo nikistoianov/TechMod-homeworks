dfa02fbe2ce7e89e160cacb4fea5b9943fcfb396
Niki-home <nikmastera@gmail.com>
2017-10-31 00:17:46 +0200

    RegEx - Update

--- RegularExpressions/KeyReplacer/05.StartUp.cs
+++ RegularExpressions/KeyReplacer/05.StartUp.cs
@@ -11,7 +11,33 @@ namespace KeyReplacer
     {
         static void Main()
         {
+            string keyString = Console.ReadLine();
+            string text = Console.ReadLine();
+            string[] keys = Regex.Split(keyString, $"[|\\<]");
+            string result = "";
 
+            if (!Regex.IsMatch(keys[0], @"^[A-Za-z]+$"))
+            {
+                //throw new Exception();
+                Console.WriteLine("Empty result");
+                return;
+            }
+
+            string pattern = $@"{keys[0]}(.*?){keys.Last()}";
+            foreach (Match m in Regex.Matches(text, pattern))
+            {
+                //Console.Write(m.Groups[1]);
+                result += m.Groups[1];
+            }
+
+            if (result == "")
+            {
+                Console.WriteLine("Empty result");
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
         }
     }
 }

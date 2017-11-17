f4d85b591d537bbd6998146b0baf43af91e33849
Nikolay <nikmastera@gmail.com>
2017-11-17 15:36:18 +0200

    JS Basic final

--- JavaScriptSyntaxAndBasicWeb/functions.js
+++ JavaScriptSyntaxAndBasicWeb/functions.js
@@ -52,4 +52,119 @@ function PrintLines(lines) {
     }
 }
 
-PrintLines(['1','2','4','Stop','rrr'])
+function PrintReversed(lines) {
+    for (let i = lines.length - 1; i >= 0; i--) {
+        console.log(lines[i]);
+    }
+}
+
+function SetValuesToIndexesArray(lines) {
+    let arrLen = Number(lines[0]);
+    let arr = Array.from('0' .repeat(arrLen));
+
+    for (let i = 1; i < lines.length; i++) {
+        let split = lines[i].split(" - ");
+
+        arr[Number(split[0])] = split[1];
+    }
+
+    for (let element of arr) {
+        console.log(element);
+    }
+}
+
+function AddRemoveElements(lines) {
+    let arr = [];
+    for (let line of lines) {
+        let split = line.split(' ');
+        if (split[0] == "add") {
+            arr.push(split[1])
+        } else if (split[0] == "remove") {
+            arr.splice(Number(split[1]), 1)
+        }
+    }
+
+    for (let element of arr) {
+        console.log(element);
+    }
+}
+
+function WorkingWithKeyValuePairs(lines) {
+    let dict = {};
+    for (let i = 0; i < lines.length - 1; i++) {
+        let split = lines[i].split(' ');
+        dict[split[0]] = split[1];
+    }
+
+    let lastKey = lines[lines.length - 1];
+    if (lastKey in dict) {
+        console.log(dict[lastKey]);
+    } else {
+        console.log('None');
+    }
+}
+
+function MultipleValuesForKey(lines) {
+    let dict = {};
+    for (let i = 0; i < lines.length - 1; i++) {
+        let split = lines[i].split(' ');
+        if (split[0] in dict) {
+            dict[split[0]].push(split[1]);
+        } else {
+            dict[split[0]] = [split[1]];
+        }
+    }
+
+    let lastKey = lines[lines.length - 1];
+    if (lastKey in dict) {
+        for (let element of dict[lastKey]) {
+            console.log(element);
+        }
+    } else {
+        console.log('None');
+    }
+}
+
+function StoringObjects(lines) {
+    let students = [];
+    for (let line of lines) {
+        let split = line.split(' -> ');
+        students.push({ name : split[0], age : split[1], grade : split[2]})
+    }
+
+    for (let student of students) {
+        console.log("Name: " + student['name']);
+        console.log("Age: " + student['age']);
+        console.log("Grade: " + student['grade']);
+    }
+}
+
+function ParseJSONObjects(lines) {
+    let students = [];
+    for (let line of lines) {
+        let student = JSON.parse(line);
+        students.push(student)
+    }
+
+    for (let student of students) {
+        console.log("Name: " + student['name']);
+        console.log("Age: " + student['age']);
+        console.log("Date: " + student['date']);
+    }
+}
+
+function TurnObjectIntoJSONString(lines) {
+    let dict = {};
+    for (let line of lines) {
+        let split = line.split(' -> ');
+        let value = Number(split[1]);
+        if (Number.isNaN(value)) {
+            value = split[1];
+        }
+        dict[split[0]] = value;
+    }
+    let str = JSON.stringify(dict);
+    console.log(str);
+}
+
+TurnObjectIntoJSONString(['name -> Angel', 'surname -> Georgiev', 'age -> 20', 'grade -> 6.00'])

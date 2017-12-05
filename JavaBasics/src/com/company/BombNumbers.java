0a692a5c92d002f3111834faaf0ad9445b5266a5
Niki-home <nikmastera@gmail.com>
2017-12-06 01:24:49 +0200

    JavaBasics

---
+++ JavaBasics/src/com/company/BombNumbers.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BombNumbers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}

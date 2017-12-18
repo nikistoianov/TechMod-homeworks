0ced33f00a41121600abfd67fc35c015948db86d
Nikolay <nikmastera@gmail.com>
2017-12-18 17:09:09 +0200

    Blog SCharp

---
+++ BlogSharp/Blog/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: Article/List
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
